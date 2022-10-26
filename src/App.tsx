afcc9dbb14a99d7562e8f269e1deb5bd59e3668c
dooffyy <muhammadkharismam@gmail.com>
2022-10-26 14:43:37 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <button className="btn bg-rose-500 px-3 py-1 mx-auto text-center capitalize rounded text-white">Hello</button>
+    </div>
+  )
+}
+
+export default App
