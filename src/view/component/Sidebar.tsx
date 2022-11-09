f6bf6aca162c2d3359c12c10762381cd8669b56b
dooffyy <muhammadkharismam@gmail.com>
2022-11-09 09:02:16 +0800

    sidebar component

---
+++ src/view/component/Sidebar.tsx
@@ -0,0 +1,42 @@
+import { Paper, MenuList, MenuItem, ListItemIcon, Typography, Divider, Box, Link } from "@mui/material";
+
+import { GridView, Style, LocalLibrary, AutoStoriesSharp, Logout } from "@mui/icons-material";
+
+
+function Sidebar(){
+    return (
+        <>
+            <Paper elevation={0} variant="outlined" sx={{ border: 0, bgcolor:"#eceff1", height: "80vh", borderRadius: 5, boxShadow: 5, position: "sticky", top: 98 , marginY: "auto"}}>
+                <MenuList sx={{height: "100%", paddingTop: 2}}>
+                    <MenuItem color="primary">
+                        <ListItemIcon><GridView/></ListItemIcon>
+                        <Typography variant="body2">Dashboard</Typography>
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon><Style/></ListItemIcon>
+                        <Typography variant="body2">Data Skripsi</Typography>
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon><LocalLibrary/></ListItemIcon>
+                        <Typography variant="body2">Data Mahasiswa</Typography>
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon><AutoStoriesSharp/></ListItemIcon>
+                        <Typography variant="body2">Data Pengajuan Buku</Typography>
+                    </MenuItem>
+                    <Divider/>
+                    <Box sx={{position: "absolute", right: 0, left: 0, bottom: 50}}>
+                    <Link underline="none" color={"inherit"} href="/login">
+                    <MenuItem>
+                        <ListItemIcon><Logout/></ListItemIcon>
+                        <Typography variant="body2">Logout</Typography>
+                    </MenuItem>
+                    </Link>
+                    </Box>
+                </MenuList>
+            </Paper> 
+        </>
+    )
+}
+
+export default Sidebar;
--- src/view/pages/LandingPage.tsx
+++ src/view/pages/LandingPage.tsx
@@ -7,14 +7,9 @@ import { GridView, Logout, Style, LocalLibrary, AutoStoriesSharp } from "@mui/ic
 
 import styles from "../../styled/GlassEffectWhite.module.css"
 import NavigationBar from "../component/NavigationBar";
+import Sidebar from "../component/Sidebar";
 
 function LandingPage(){
-
-    const [isActive, setIsActive] = useState(false)
-
-    function ActiveMenu(){
-        setIsActive(!isActive)
-    }
 
     return (
         <>
@@ -22,36 +17,13 @@ function LandingPage(){
             
             {/* computer view */}
             <Grid container spacing={2} paddingX={2} marginTop={0.1}>
-                <Grid xs={12} item md={4} lg={2.5} position="relative">
-                    <Paper elevation={0} variant="outlined" sx={{ border: 0, bgcolor:"#eceff1", height: "80vh", borderRadius: 5, boxShadow: 5, position: "sticky", top: 98 , marginY: "auto"}}>
-                        <MenuList sx={{height: "100%", paddingTop: 2}}>
-                            <MenuItem color="primary" selected={isActive} onClick={ActiveMenu}>
-                                <ListItemIcon><GridView/></ListItemIcon>
-                                <Typography variant="body2">Dashboard</Typography>
-                            </MenuItem>
-                            <MenuItem>
-                                <ListItemIcon><Style/></ListItemIcon>
-                                <Typography variant="body2">Data Skripsi</Typography>
-                            </MenuItem>
-                            <MenuItem>
-                                <ListItemIcon><LocalLibrary/></ListItemIcon>
-                                <Typography variant="body2">Data Mahasiswa</Typography>
-                            </MenuItem>
-                            <MenuItem>
-                                <ListItemIcon><AutoStoriesSharp/></ListItemIcon>
-                                <Typography variant="body2">Data Pengajuan Buku</Typography>
-                            </MenuItem>
-                            <Divider/>
-                            <Box sx={{position: "absolute", right: 0, left: 0, bottom: 50}}>
-                            <Link underline="none" color={"inherit"} href="/login">
-                            <MenuItem>
-                                <ListItemIcon><Logout/></ListItemIcon>
-                                <Typography variant="body2">Logout</Typography>
-                            </MenuItem>
-                            </Link>
-                            </Box>
-                        </MenuList>
-                    </Paper>              
+                <Grid xs={12} item md={4} lg={2.5} position="relative" sx={{
+                    display: {
+                        xs: 'none',
+                        sm: 'block'
+                    }
+                }}>
+                    <Sidebar/>             
                 </Grid>
                 <Grid item xs={12} md={8} lg={9}>
                     <Typography>Testing</Typography>
