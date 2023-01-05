6b6c974426abe865fdc9308745f88c0ea3cd8949
dooffyy <muhammadkharismam@gmail.com>
2023-01-05 15:56:36 +0800

    deploy after settings router

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  define: {
-    'process.env.NODE_ENV': `${process.env.NODE_ENV}`
-  }
+  // define: {
+  //   'process.env.NODE_ENV': `${process.env.NODE_ENV}`
+  // }
 })
